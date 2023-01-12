0f56709572f7e2e93c85444bcc2a93a30a18fbb9
Kostiantyn <konstantinbelenko36@gmail.com>
2023-01-12 17:10:35 +0100

    :bug: Fixed view for non-authed users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,9 +12,10 @@ class HomeController extends Controller
         /* Send welcome view with all polls that are open (paginated), desc order */
 
         $all_posts = Poll::where('is_open', true)->orderBy('created_at', 'desc')->paginate(10);
+        $authed = auth()->check();
 
         // Remove admin polls if user is not admin or owner
-        if (!auth()->user()->is_admin && !auth()->user()->is_owner) {
+        if ($authed && !auth()->user()->is_admin && !auth()->user()->is_owner) {
             $all_posts = $all_posts->filter(function ($poll) {
                 // Remove admin polls using poll table 'type'
                 return $poll->type != 'admin';
@@ -22,7 +23,7 @@ class HomeController extends Controller
         }
 
         // Remove student polls if user is not student
-        if (!auth()->user()->is_approved) {
+        if ($authed && !auth()->user()->is_approved) {
             $all_posts = $all_posts->filter(function ($poll) {
                 // Remove student polls using poll table 'type'
                 return $poll->type != 'student';
