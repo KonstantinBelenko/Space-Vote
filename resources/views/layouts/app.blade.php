500adb167c7fb4c2df14db49fd0f0bc7332a135d
Kostiantyn <konstantinbelenko36@gmail.com>
2023-01-16 13:20:22 +0100

    :bug: Fixed chart sizes (allegedly)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
             <!-- Page Heading -->
             <a href="{{ route('home') }}">
                 <header class="w-full h-16 p-4 pl-6">
-                    <span class="font-bold text-xl">🌑 Space Vote</span>
+                    <span class="fixed font-bold text-xl">🌑 Space Vote</span>
                     <div class="relative flex items-top justify-center items-center py-4 sm:pt-0">
                         @if (Route::has('login'))
                             <div class="fixed top-0 right-0 px-6 py-4 block">
