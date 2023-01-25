88f371dccd60a515ac64d5d925e9eee80b78f12f
Kostiantyn <konstantinbelenko36@gmail.com>
2023-01-25 19:00:32 +0100

    :)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,7 +47,7 @@
             {{-- Get rainbow button --}}
             <form method="GET" action="{{ route('getpremium') }}" class="max-w-2xl mx-auto px-6 mt-4">
                 @csrf
-                <button class="border-2 border-green-500 hover:border-green-300 w-full text-green-500 hover:text-green-300 text-xl  px-4 py-2 rounded-xl transition-all duration-300">GET THE GAY VERSION</button>
+                <button class="border-2 border-green-500 hover:border-green-300 w-full text-green-500 hover:text-green-300 text-xl  px-4 py-2 rounded-xl transition-all duration-300">GET THE RAINBOW</button>
             </form>
             @endif
 
